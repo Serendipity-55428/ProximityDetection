1ce186b6b34d500558a5fbefeeacd7c056198aab
xiaosonggege <34638887+xiaosonggege@users.noreply.github.com>
2018-12-23 23:19:47 +0800

    代码有修改

--- GBDT_RNN/GBDT_Strategy.py
+++ GBDT_RNN/GBDT_Strategy.py
@@ -9,15 +9,19 @@
 @time: 2018/12/21 23:26
 @desc:
 '''
-import xgboost as xgb
-import numpy as np
-from xgboost import plot_importance
+
 from matplotlib import pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+import xgboost as xgb
 
 #数据集分为：训练集：测试集= 5:2，并且训练集分为5份做交叉验证
 #数据从文件中读取
-train_data = []
-test_data = []
+# train_data = []
+# test_data = []
 
 def data_generator():
     '''
@@ -26,55 +30,67 @@ def data_generator():
     :return: 划分好的数据-标签组合
     '''
 
-def xgboost_main():
-    '''GBDT主函数'''
+model = XGBRegressor(
+    max_depth= 5, #树的最大深度
+    learning_rate= 0.1, #学习率
+    n_estimators= 100, #树的个数
+    objective= 'reg:gamma', #损失函数类型
+    nthread= 4, #线程数
+    gamma= 0.1, #节点分裂时损失函数所需最小下降值
+    min_child_weight= 1, #叶子结点最小权重
+    subsample= 1., #随机选择样本比例建立决策树
+    colsample_bytree= 1., #随机选择样本比例建立决策树
+    reg_lambda= 3, #二阶范数正则化项权衡值
+    scale_pos_weight= 1., #解决样本个数不平衡问题
+    random_state= 1000, #随机种子设定值
+)
 
-    #训练集均分为5组做5折交叉验证
-    for fold in range(5):
-        train_feature, train_target = data_generator()
+# train_feature, train_target = data_generator()
+# test_feature, test_target = data_generator()
 
-        #设置GBDT所需参数散列
-        GBDT_params = {
-            'booster': 'gbtree', #所选择的若学习器算法框架
-            'objective': 'reg:linear', #所选择的回归损失函数
-            'gamma': 0.1, #指定节点分裂所需的最小损失函数下降值
-            'max_depth': 5, #书的最大深度
-            'lambda': 3, #正则化项参数
-            'subsample': 0.7, #控制对于每一棵树随机采样的比例
-            'colsample_bytree': 0.7, #和subsample作用相同
-            'min_child_weight': 3., #决定最小叶子节点样本权重和
-            'silent': 0, #默认为0，当参数值为1时，静默模式开启，不会输出任何信息
-            'eta': 0.1, #控制每一步权重减小的参数0.01-0.2
-            'seed': 1000, #随机数种子
-            'nthread': 4 #进行多线程控制
-        }
+#载入波士顿房价预测数据集做测试
+boston = datasets.load_boston()
+train_feature, test_feature, train_target, test_target = train_test_split(boston.data, boston.target, test_size= 0.333,
+                                                                          random_state= 42)
 
-        #将数据特征和标签存为DMatrix类型数据
-        dtrain = xgb.DMatrix(train_feature, train_target)
-        #设置集成弱学习器数量(需根据自行设定误差限设定)
-        num_boost_round = 100
-        #构建模型并训练
-        model = xgb.train(params= GBDT_params, dtrain= dtrain,
-                          num_boost_round= num_boost_round)
+#训练数据
+model.fit(X= train_feature,
+          y= train_target,
+          # eval_set= [(test_feature, test_target)], #交叉验证集特征和标签
+          # early_stopping_rounds= 10, #模型多少次得分基本不变就停止训练
+          # verbose= True, #是否使用验证集
+          )
 
-        #保存模型
-        model.save_model('GBDT.model')
-        #导出模型和特征映射到txt文件
-        model.dump_model('dump.raw.txt', 'featmap.txt')
+#模型预测计算精确度
+pred_target = model.predict(test_feature)
+# print(pred_target)
+ex_var = explained_variance_score(test_target, pred_target) #解释方差：1-(var(y_pred-y)/var(y_pred))
+print('解释方差为: %s' % ex_var)
+me_ab = mean_absolute_error(test_target, pred_target) #街区距离
+print('街区距离为: %s' % me_ab)
+me_sq = mean_squared_error(test_target, pred_target)
+print('均方差为: %s' % me_sq)
 
-        #加载模型
-        #init model
-        model = xgb.Booster({'nthread': 4})
-        #load data
-        model.load_model('model.bin')
 
-        #对测试集预测
-        test_feature, test_target = data_generator()
-        predict_test = model.predict(test_feature)
+#保存模型
+# model.get_booster().save_model('GBDT.model')
 
-        #显示重要特征
-        plot_importance(model)
-        plt.show()
+#显示重要参数
+# plot_importance(model)
+# plt.show()
+
+#加载模型
+# model = xgb.Booster(model_file= 'GBDT.model')
+
+
+
+
+
+
+
+
+
+
 
 
 
