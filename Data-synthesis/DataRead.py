549870848406b361f5b63de2be4deadbe881ce35
xiaosonggege <1243049371@qq.com>
2019-06-29 20:27:52 +0800

    代码有修改

--- Data-synthesis/DataRead.py
+++ Data-synthesis/DataRead.py
@@ -14,7 +14,7 @@ import pickle
 import numpy as np
 import xlwt
 import pandas as pd
-from Stacking.Routine_operation import LoadFile
+# from Stacking.Routine_operation import LoadFile
 from collections import Counter
 
 def GeneratorData(type, filename_prefix):
@@ -26,19 +26,19 @@ def GeneratorData(type, filename_prefix):
     '''
     movingObjects = [1000, 1500, 2000, 4000, 6000, 8000, 10000, 20000, 40000, 60000, 80000, 100000]
     maxSpeed = ['v' + '%s' % i for i in range(1, 7)]
-    maxSpeed = list(zip(maxSpeed, [143.022339, 143.022339, 28.593955, 28.593955, 5.703022, 5.703022]))  # 改
+    maxSpeed = list(zip(maxSpeed, [143.022339, 143.022339, 28.593955, 28.593955, 5.703022, 5.703022]))
     friends = [5, 10, 20, 30, 40]
     Threshold = [i for i in range(1, 11)]
     #初始化待输出numpy数据矩阵
     dataset = np.zeros(shape=(1, 25))
-    for file_movingObjects in movingObjects: #改
+    for file_movingObjects in movingObjects:
         for file_maxSpeed in maxSpeed:  # file_maxSpeed类型为元组 #改
             for file_friends in friends:
                 for file_Threshold in Threshold:
                     # 前4个特征列表，需要和文件中20个特征、1个最优半径结合后转换为ndarray类型向量
                     sub_feature = [file_movingObjects, file_maxSpeed[-1], file_friends, file_Threshold]
                     # 数据文件命名格式：type_movingObjects_maxSpeed_friends_Threshold
-                    p = filename_prefix + '\\' + r'%d_%d_%s_%d_%d.dat' % \
+                    p = filename_prefix + '/' + r'%d_%d_%s_%d_%d.dat' % \
                         (type, file_movingObjects, file_maxSpeed[0], file_friends, file_Threshold)
                     if os.path.exists(p):
                         with open(p, 'r') as f:
@@ -58,7 +58,7 @@ def GeneratorData(type, filename_prefix):
                                     # 指向下一个r中同一时间的cost
                                     point += 17
 
-                                # min_cost位置回退2便指向对应20个密度特征，回退3便指向当前最优半径
+                                # min_cost位置回退2便指向对应20个密度特征，回退(ts + 1) * 3个便指向当前最优半径
                                 twenty_feature = file[min_cost - 2][:-2]
                                 twenty_feature = twenty_feature.split(' ')
                                 twenty_feature = [float(i) for i in twenty_feature] if len(
@@ -125,28 +125,28 @@ def checkclassifier(vector):
 if __name__ == '__main__':
     rng = np.random.RandomState(2)
     #生成pny数据
-    # type= 1
-    # filename_prefix = r'C:\Users\xiaosong\Desktop\pny'
-    # dataset = GeneratorData(type= type, filename_prefix= filename_prefix)
-    # rng.shuffle(dataset)
+    type= 1
+    filename_prefix = r'/home/xiaosong/pny'
+    dataset = GeneratorData(type= type, filename_prefix= filename_prefix)
+    rng.shuffle(dataset)
 
     #检查最优半径为0.01的数量
-    # zero_r, nonzero_r = 0, 0
-    # r = dataset[:, -1]
-    # for i in r:
-    #     if i == 0.01:
-    #         zero_r += 1
-    # nonzero_r = len(r) - zero_r
-    # print('最优半径为0.01个数: %s \n 最优半径不为0.01的个数: %s' % (zero_r, nonzero_r))
-    # # print(dataset)
-    # print(dataset.shape)
-    # print(dataset.dtype)
+    zero_r, nonzero_r = 0, 0
+    r = dataset[:, -1]
+    for i in r:
+        if i == 0.01:
+            zero_r += 1
+    nonzero_r = len(r) - zero_r
+    print('最优半径为0.01个数: %s \n最优半径不为0.01的个数: %s' % (zero_r, nonzero_r))
+    # print(dataset)
+    print(dataset.shape)
+    print(dataset.dtype)
 
     #保存文件到.xlsx/.pickle
-    save_pickle = r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_all.pickle'
-    save_xlsx = r'F:\ProximityDetection\Stacking\dataset_PNY\datasets.xlsx'
-    # Numpy2Excel(data= dataset, save_p= save_xlsx)
-    # SaveFile(data=dataset, savepickle_p=save_pickle)
+    save_pickle = r'/home/xiaosong/桌面/PNY_all.pickle'
+    save_xlsx = r'/home/xiaosong/桌面/datasets.xlsx'
+    Numpy2Excel(data= dataset, save_p= save_xlsx)
+    SaveFile(data=dataset, savepickle_p=save_pickle)
 
     #划分最优半径含0.01和不含0.01的数据
     # data_all_ = LoadFile(p=save_pickle)
@@ -159,17 +159,17 @@ if __name__ == '__main__':
     # print(np.array(data_no_noise).shape, np.array(data_noise).shape)
     # SaveFile(data=data_noise, savepickle_p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_noise.pickle')
     # SaveFile(data=data_no_noise, savepickle_p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_no_noise.pickle')
-    data_noise = LoadFile(p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_noise.pickle')
-    data_no_noise = LoadFile(r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_no_noise.pickle')
+    # data_noise = LoadFile(p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_noise.pickle')
+    # data_no_noise = LoadFile(r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_no_noise.pickle')
     # print(data_noise.shape, data_no_noise.shape)
     #制作数量为10000的数据集，无噪声数据集数量不够可以用少量噪声数据集进行填充
-    data_train = np.vstack((data_no_noise, data_noise[:(10000-data_no_noise.shape[0]), :]))
-    rng.shuffle(data_train)
+    # data_train = np.vstack((data_no_noise, data_noise[:(10000-data_no_noise.shape[0]), :]))
+    # rng.shuffle(data_train)
     # SaveFile(data=data_train, savepickle_p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_data_train.pickle')
     # print(data_train.shape)
-    # statistic = Counter(data_train[:, -1])
-    # for key, value in statistic.items():
-    #     print('%s: %s' % (key, value))
+    statistic = Counter(dataset[:, -1])
+    for key, value in statistic.items():
+        print('%s: %s' % (key, value))
     #制作平均密度序列的fft变换膜值序列
     # data_fft_noise = np.hstack((np.abs(np.fft.fft(a=data_noise[:, 4:-1], n=100, axis=1)),data_noise[:, :4], data_noise[:, -1][:, np.newaxis]))
     # data_fft_no_noise = np.hstack((np.abs(np.fft.fft(a=data_no_noise[:, 4:-1], n=100, axis=1)),data_no_noise[:, :4], data_no_noise[:, -1][:, np.newaxis]))
@@ -183,9 +183,9 @@ if __name__ == '__main__':
     # print(data_fft.shape)
 
     #对原始数据和经过傅里叶变换后的数据进行列归一化
-    data_PNY = LoadFile(p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_data_train.pickle')
-    data_PNY = (data_PNY - np.min(data_PNY, axis=0)) / (np.max(data_PNY, axis=0) - np.min(data_PNY, axis=0))
-    checkclassifier(data_PNY[:, -1])
+    # data_PNY = LoadFile(p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_data_train.pickle')
+    # data_PNY = (data_PNY - np.min(data_PNY, axis=0)) / (np.max(data_PNY, axis=0) - np.min(data_PNY, axis=0))
+    # checkclassifier(data_PNY[:, -1])
     # SaveFile(data=data_PNY, savepickle_p=r'F:\ProximityDetection\Stacking\dataset_PNY\PNY_data_norm.pickle')
 
 
